2371dde3c0fee5c4acfd07eb531f36ce80d14955
Derek DeLizo <delizoderek@gmail.com>
2022-06-18 15:11:22 -0700

    Updated styling for tools image, table of contents buttons, spacing between buttons and website tools section, and Added footer to all pages

--- public/js/constants.js
+++ public/js/constants.js
@@ -3,7 +3,7 @@ const cardTemplate = ({type, desc, url, imgSrc}) => `
   <img src="./public/img/${imgSrc}" class="mt-4 card-img-top w-75" alt="${type} Logo"/>
   <div class="d-flex card-body flex-column align-items-start">
     <h5 class="card-title text-quaternary">${type}</h5>
-    <p class="text-tertiary flex-grow-1 text-medium">${desc}</p>
+    <p class="text-tertiary flex-grow-1 text-medium text-start">${desc}</p>
     <a href="${url}" class="btn color-primary text-quaternary" target="_blank">Build with ${type}</a>
   </div>
 </div>
